499768a76b45425dec66b5f43f11b93e0e233e2d
fbawa <50893833+fbawa@users.noreply.github.com>
2019-06-29 18:12:19 -0400

    Selenium + BS

--- sales-leads.py
+++ sales-leads.py
@@ -1,7 +1,8 @@
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+import requests
+from bs4 import BeautifulSoup
 
 #initialize driver
 
@@ -26,14 +27,67 @@ searchbox = driver.find_element_by_xpath(searchbox_xpath)
 # INTERACT WITH THE ELEMENT
 #
 
-search_term = "Benoit Electric"
+search_term = "Pepsi"
 searchbox.send_keys(search_term)
 
 searchbox.send_keys(Keys.RETURN)
-print(driver.title) #> 'Prof Rossetti GitHub - Google Search'
+#print(driver.title) #> 'Prof Rossetti GitHub - Google Search'
 driver.save_screenshot("search_results.png")
+
 
 #
 # ALWAYS QUIT THE DRIVER
 #
-print(type(search_term))
+#response = requests.get("https://www.buzzfile.com/Search/Company/Results?searchTerm=Pepsi&amp;type=1")
+#response_html = response.text
+
+#soup = BeautifulSoup(response_html, features = "lxml")
+soup = BeautifulSoup(driver.page_source, features = "lxml")
+
+#titles = soup.find_all("span", "title")
+
+#print(type(titles)) #> <class 'bs4.element.ResultSet'> (like a list)
+#print(titles[5]) #> <span class="title">Macbeth</span>
+#print(titles[5].text) #> Macbeth
+#exit()
+
+companylinks = soup.find_all("span", "search-display-info") # class name and what class equals
+
+print(companylinks)
+
+
+
+
+exit()
+books = []
+for list_item in booklinks:
+    title = list_item.find("span", "title").text #> class name and what class equals
+    author = list_item.find("span", "subtitle").text #> "William Shakespeare"
+    downloads = list_item.find("span", "extra").text #> '830 downloads'
+    downloads_count = int(downloads.replace(" downloads", "")) #> 830
+    book = {"title": title, "author": author, "downloads": downloads_count}
+    print(book)
+    books.append(book)
+
+print(books[2]["title"]) #> Macbeth
+
+
+#booklinks = soup.find_all("li", "booklink") # class name and what class equals
+
+#books = []
+#for list_item in booklinks:
+#    title = list_item.find("span", "title").text #> class name and what class equals
+#   author = list_item.find("span", "subtitle").text #> "William Shakespeare"
+ #   downloads = list_item.find("span", "extra").text #> '830 downloads'
+  #  downloads_count = int(downloads.replace(" downloads", "")) #> 830
+   # book = {"title": title, "author": author, "downloads": downloads_count}
+    #print(book)
+    #books.append(book)
+
+#print(books[2]["title"]) #> Macbeth
+
+
+
+
+
+
